d61153ec83cc7f1d2d2520915b75bdfb47fa7c67
TheOneMoritz <61712524+TheOneMoritz@users.noreply.github.com>
2021-10-09 14:48:43 +0200

    Update

---
+++ js/build/web/_next/static/G-4p99qEqjqmITZXsdoNf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-99332ec305abd159d5fe.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
